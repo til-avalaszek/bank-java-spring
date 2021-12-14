dfec3923e341f0294d4f82358df153ae531f45eb
Adilton Valaszek <valaszek@gmail.com>
2021-12-14 13:19:22 -0300

    test: add more unit test to account

--- src/test/java/com/valaszek/bank/AccountServiceUnitTest.java
+++ src/test/java/com/valaszek/bank/AccountServiceUnitTest.java
@@ -1,9 +1,10 @@
 package com.valaszek.bank;
 
+import com.valaszek.bank.account.AccountService;
 import com.valaszek.bank.account.dto.AccountDto;
 import com.valaszek.bank.account.model.AccountEntity;
 import com.valaszek.bank.account.repository.AccountRepository;
-import com.valaszek.bank.account.AccountService;
+import com.valaszek.bank.exception.AccountNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,12 +16,15 @@ import java.math.BigInteger;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class AccountServiceUnitTest {
+
+  private static final BigInteger ACCOUNT_ID = BigInteger.valueOf(1);
 
   @Mock private AccountRepository accountRepository;
 
@@ -37,30 +41,30 @@ public class AccountServiceUnitTest {
 
   @Test
   void shouldReturnAccount() {
-    when(accountRepository.findById(BigInteger.valueOf(1))).thenReturn(mockAccount());
-    var account = accountService.findAccount(BigInteger.valueOf(1));
+    when(accountRepository.findById(ACCOUNT_ID)).thenReturn(mockAccount());
+    var account = accountService.findAccount(ACCOUNT_ID);
 
     assertThat(account).isNotNull();
-    Assertions.assertEquals(BigInteger.valueOf(1), account.getAccountId());
+    Assertions.assertEquals(ACCOUNT_ID, account.getAccountId());
   }
 
   @Test
-  void shouldThrowsExceptionAccountNotFound() {
+  public void shouldThrowsExceptionAccountNotFound() throws Exception {
+    when(accountRepository.findById(ACCOUNT_ID)).thenThrow(AccountNotFoundException.class);
 
+    assertThrows(
+        AccountNotFoundException.class,
+        () -> {
+          accountService.findAccount(ACCOUNT_ID);
+        });
   }
 
   private AccountDto mockAccountDto() {
-    return AccountDto.builder()
-        .accountId(BigInteger.valueOf(1))
-        .documentNumber("692.265.440-42")
-        .build();
+    return AccountDto.builder().accountId(ACCOUNT_ID).documentNumber("692.265.440-42").build();
   }
 
   private Optional<AccountEntity> mockAccount() {
     return Optional.of(
-        AccountEntity.builder()
-            .accountId(BigInteger.valueOf(1))
-            .documentNumber("692.265.440-42")
-            .build());
+        AccountEntity.builder().accountId(ACCOUNT_ID).documentNumber("692.265.440-42").build());
   }
 }
